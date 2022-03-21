00107d1713455ea39ae240fbe990e6522c25a671
unknown <palo.loui.andrae@iisgalvanimi.edu.it>
2022-03-21 22:51:31 +0100

    (fix) rm AsyncStorage warning

--- App.tsx
+++ App.tsx
@@ -1,5 +1,10 @@
 import AppContainer from "./src/components/app-container";
 import Navigator from "./src";
+import { LogBox } from "react-native";
+
+LogBox.ignoreLogs([
+  "AsyncStorage has been extracted from react-native core and will be removed in a future release. It can now be installed and imported from '@react-native-async-storage/async-storage' instead of 'react-native'. See https://github.com/react-native-async-storage/async-storage",
+]);
 
 export default function App() {
   return (
